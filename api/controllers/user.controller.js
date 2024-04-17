e0944989d485452950bd19df598eb5a97bd9b16d
Denio Nhanale <dnhanale@gmail.com>
2024-04-17 16:51:28 +0200

    Add Update user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -23,7 +23,7 @@ export const updateUser = async (req, res, next) => {
           username: req.body.username,
           email: req.body.email,
           password: req.body.password,
-          avatar: req.body.aatar,
+          avatar: req.body.avatar,
         },
       },
       { new: true }
