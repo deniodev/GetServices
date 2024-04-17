460ec2a20dacf8e2211ac423b98d470b1625ac9c
Denio Nhanale <dnhanale@gmail.com>
2024-04-17 10:39:12 +0200

    Add Google OAuth to server

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -2,9 +2,11 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { app } from "../firebase";
 import { useDispatch } from "react-redux";
 import { signInSuccess } from "../redux/user/userSlice";
+import {useNavigate} from "react-router-dom";
 
 const OAuth = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
@@ -25,6 +27,7 @@ const OAuth = () => {
       });
       const data = await res.json();
       dispatch(signInSuccess(data));
+        navigate('/')
     } catch (error) {
       console.log("could not sign in with google", error);
     }
