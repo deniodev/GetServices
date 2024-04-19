c97a29d2224d7093edb358889d340e9fd013afa8
Denio Nhanale <dnhanale@gmail.com>
2024-04-19 16:24:49 +0200

    Add Search api route

--- api/controllers/service.controller.js
+++ api/controllers/service.controller.js
@@ -44,7 +44,7 @@ export const updateService = async (req, res, next) => {
     const updatedService = await Service.findByIdAndUpdate(
       req.params.id,
       req.body,
-      {new: true}
+      { new: true }
     );
     res.status(200).json(updatedService);
   } catch (error) {
@@ -56,10 +56,61 @@ export const getService = async (req, res, next) => {
   try {
     const service = await Service.findById(req.params.id);
     if (!service) {
-      return next(errorHandler(404, 'Service not found!'))
+      return next(errorHandler(404, "Service not found!"));
     }
     res.status(200).json(service);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
+
+export const getServices = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+
+    const sort = req.query.sort || "createdAt";
+
+    const order = req.query.order || "desc";
+
+    const services = await Service.find({
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(services);
+  } catch (error) {
+    next(error);
+  }
+};
