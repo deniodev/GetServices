11068032473ede90b07db647baae9c3c462d8dcd
Denio Nhanale <dnhanale@gmail.com>
2024-04-25 17:27:46 +0200

    Update Service Item card UI

--- client/src/components/ServiceItem.jsx
+++ client/src/components/ServiceItem.jsx
@@ -1,49 +1,32 @@
-import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { MdLocationOn } from "react-icons/md";
+import { MapPin, Star } from "lucide-react";
 
 const ServiceItem = ({ service }) => {
-  const [appointment, setAppointment] = useState(null);
-
-  useEffect(() => {
-    const fetchAppointment = async () => {
-      try {
-        const res = await fetch(`/api/user/${service.userRef}`);
-        const data = await res.json();
-        setAppointment(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchAppointment();
-  }, [service.userRef]);
-
   return (
     <>
-      <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
+      <div className="border-solid border-2  shadow-md hover:shadow-2xl transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
         <Link to={`/service/${service._id}`}>
-          {appointment && (
-            <img
-              src={
-                appointment.avatar ||
-                "https://i0.wp.com/florrycreativecare.com/wp-content/uploads/2020/08/blank-profile-picture-mystery-man-avatar-973460.jpg?ssl=1"
-              }
-              alt="service cover"
-              className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
-            />
-          )}
+          <img
+            src={
+              service.coverImg ||
+              "https://i0.wp.com/florrycreativecare.com/wp-content/uploads/2020/08/blank-profile-picture-mystery-man-avatar-973460.jpg?ssl=1"
+            }
+            alt="service cover"
+            className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
+          />
+
           <div className="p-3 flex flex-col gap-2 w-full">
-            <p className="truncate text-lg font-semibold text-slate-700">
-              {service.name}
-            </p>
-            <p className="text-sm text-gray-600 line-clamp-2">
-              {service.description}
-            </p>
+            <p className="truncate text-lg font-bold">{service.name}</p>
+            <div className="flex items-center justify-between">
+            <p className="text-sm  font-semibold">{service.title}</p>
+            <div className="flex items-center gap-[6px]">
+            <Star size={15}/> <span className="font-semibold">3.5</span>
+            </div>
+             
+            </div>
             <div className="flex items-center gap-1">
-              <MdLocationOn className="h-4 w-4 text-green-700" />
-              <p className="text-sm text-gray-600 truncate w-full">
-                {service.address}
-              </p>
+              <MapPin size={15} />
+              <p className="text-sm truncate w-full">{service.city}</p>
             </div>
           </div>
         </Link>
