a67265d4a7cd01b676ac8b8bb94baafc1f5b1f3c
Denio Nhanale <dnhanale@gmail.com>
2024-05-13 11:08:25 +0200

    Add Loading State to Send Message Button

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,29 +1,31 @@
-import { useRef } from 'react';
-import emailjs from '@emailjs/browser';
-import toast from 'react-hot-toast';
-import { useTranslation } from 'react-i18next';
-import { Label } from '../components/ui/label';
-import { Input } from '../components/ui/input';
-import { Textarea } from '../components/ui/textarea';
-import { Button } from '../components/ui/button';
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import toast from "react-hot-toast";
+import { useTranslation } from "react-i18next";
+import { Label } from "../components/ui/label";
+import { Input } from "../components/ui/input";
+import { Textarea } from "../components/ui/textarea";
+import { Button } from "../components/ui/button";
+import { Loader2 } from "lucide-react";
 
 const Contact = () => {
   const { t } = useTranslation();
-
+  const [loading, setLoading] = useState(false);
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
-
+    setLoading(true);
     emailjs
       .sendForm(
-        'service_up3dqi5',
-        'template_0lz9f1d',
+        "service_up3dqi5",
+        "template_0lz9f1d",
         form.current,
-        'r10s-hMpE6DwP6_gw',
+        "r10s-hMpE6DwP6_gw"
       )
       .then(() => {
-        toast.success('Message sent!');
+        setLoading(false);
+        toast.success("Message sent!");
         e.target.reset();
       });
   };
@@ -32,15 +34,14 @@ const Contact = () => {
     <section>
       <div className="p-3 max-w-4xl mx-auto">
         <h1 className="text-3xl font-semibold text-center p-2">
-          {' '}
-          {t('contact1')}
-          {' '}
+          {" "}
+          {t("contact1")}{" "}
         </h1>
-        <p className="text-s leading-loose text-center">{t('contact2')}</p>
+        <p className="text-s leading-loose text-center">{t("contact2")}</p>
         <form ref={form} onSubmit={sendEmail} className="space-y-8 ml-5 mr-5">
           <div>
             <Label htmlFor="name" className="ml-1">
-              {t('name')}
+              {t("name")}
             </Label>
             <Input
               type="name"
@@ -66,19 +67,19 @@ const Contact = () => {
           </div>
           <div>
             <Label htmlFor="subject" className="form__label">
-              {t('subject1')}
+              {t("subject1")}
             </Label>
             <Input
               type="text"
               name="user_subject"
               id="subject"
-              placeholder={t('subject2')}
+              placeholder={t("subject2")}
               required
             />
           </div>
           <div className="sm:col-span-2">
             <Label htmlFor="message" className="form__label">
-              {t('message')}
+              {t("message")}
             </Label>
             <Textarea
               name="message"
@@ -90,8 +91,19 @@ const Contact = () => {
               className="form__input mt-1"
             />
           </div>
-          <Button type="submit" className="btn rounded sm:w-fit">
-            {t('send')}
+          <Button
+            type="submit"
+            className="btn rounded sm:w-fit"
+            disabled={loading}
+          >
+            {loading ? (
+              <>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                {t("pleasewait")}
+              </>
+            ) : (
+              t("send")
+            )}
           </Button>
         </form>
       </div>
