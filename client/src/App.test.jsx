b11106f3cd511e20d888c9d8b1d41595b412267d
Denio Nhanale <dnhanale@gmail.com>
2024-05-13 09:42:20 +0200

    Add Test for App.jsx

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -1,14 +1,15 @@
-import { describe, expect, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from './redux/store';
-import App from './App';
-import './index.css';
-import { ThemeProvider } from './components/theme-provider';
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store";
+import App from "./App";
+import "./index.css";
+import "../src/test/setup";
+import { ThemeProvider } from "./components/theme-provider";
 
-describe('App', () => {
-  it('checking whetever service text is available', () => {
+describe("App", () => {
+  it("checking whetever service text is available", () => {
     render(
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
@@ -16,9 +17,9 @@ describe('App', () => {
             <App />
           </ThemeProvider>
         </PersistGate>
-      </Provider>,
+      </Provider>
     );
-    const text = screen.getByText('Services');
+    const text = screen.getByText("Services");
     expect(text).toBeInTheDocument();
   });
 });
