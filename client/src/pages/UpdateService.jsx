6849f0d432e4d88ceabd43fb4a68f82d9248d61d
Denio Nhanale <dnhanale@gmail.com>
2024-06-12 16:32:18 +0200

    Update Update Service Page UI

--- client/src/pages/UpdateService.jsx
+++ client/src/pages/UpdateService.jsx
@@ -77,12 +77,10 @@ const UpdateService = () => {
       );
     });
 
-  const handleCoverImageSubmit = () => {
-    if (files.length === 1) {
+  const handleCoverImageSubmit = (file) => {
+    if (file) {
       setCoverUploading(true);
       setImageUploadError(false);
-
-      const file = files[0];
 
       storeImage(file)
         .then((url) => {
@@ -103,7 +101,7 @@ const UpdateService = () => {
     }
   };
 
-  const handleImageSubmit = () => {
+  const handleImageSubmit = (files) => {
     if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
       setUploading(true);
       setImageUploadError(false);
@@ -122,13 +120,25 @@ const UpdateService = () => {
           setUploading(false);
         })
         .catch(() => {
-          setImageUploadError('Image upload failed (4 mb max per image)');
+          setImageUploadError('Image upload failed (2 mb max per image)');
           setUploading(false);
         });
     } else {
       setImageUploadError('You can only upload 6 images per service');
       setUploading(false);
     }
+  };
+
+  const handleCoverImageChange = (e) => {
+    const file = e.target.files[0];
+    setFiles([file]);
+    handleCoverImageSubmit(file);
+  };
+
+  const handleGalleryImagesChange = (e) => {
+    const selectedFiles = Array.from(e.target.files);
+    setFiles(selectedFiles);
+    handleImageSubmit(selectedFiles);
   };
 
   const handleRemoveImage = (index) => {
@@ -197,15 +207,18 @@ const UpdateService = () => {
       setLoading(false);
     }
   };
+
   return (
-    <main className="p-3 max-w-4xl mx-auto">
-      <h1 className="text-3xl font-semibold text-center p-2">
-        {' '}
-        {t('updateservice')}{' '}
-      </h1>
-      <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row gap-4">
-        <div className="flex flex-col gap-4 flex-1">
-          <div className="grid w-full max-w-sm items-center gap-1.5">
+    <section>
+      <div className="p-3 max-w-screen-xl mx-auto">
+        <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">
+          {t('updateservice')}
+        </h1>
+        <p className="leading-7 [&:not(:first-child)]:mt-6">
+          {t('createservice2')}
+        </p>
+        <form onSubmit={handleSubmit} className="space-y-4 mt-8">
+          <div className="max-w-screen-xl  items-center gap-1.5">
             <Label htmlFor="name" className="ml-1">
               {t('name')}
             </Label>
@@ -220,205 +233,189 @@ const UpdateService = () => {
               value={formData.name}
             />
           </div>
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label htmlFor="select" className="ml-1">
-              {t('city')}
-            </Label>
-            <select
-              id="city"
-              name="city"
-              onChange={handleChange}
-              value={formData.city}
-              required
-              className="dark:bg-[#0c0a09] cursor-pointer appearance-none flex h-9 w-full rounded-md border border-input  px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
-            >
-              <option value="All">{t('selectcity')}</option>
-              <option value="Pemba">Pemba</option>
-              <option value="Lichinga">Lichinga</option>
-              <option value="Nampula">Nampula</option>
-              <option value="Nacala">Nacala</option>
-              <option value="Quelimane">Quelimane</option>
-              <option value="Tete">Tete</option>
-              <option value="Moatize">Moatize</option>
-              <option value="Chimoio">Chimoio</option>
-              <option value="Beira">Beira</option>
-              <option value="Dondo">Dondo</option>
-              <option value="Maxixe">Maxixe</option>
-              <option value="Inhambane">Inhambane</option>
-              <option value="Xai-Xai">Xai-Xai</option>
-              <option value="Maputo">Maputo</option>
-              <option value="Matola">Matola</option>
-            </select>
-          </div>
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label htmlFor="phone" className="ml-1">
-              {t('phone')}
-            </Label>
-            <Input
-              type="phone"
-              placeholder={t('whatsaap')}
-              id="phone"
-              required
-              onChange={handleChange}
-              value={formData.phone}
-            />
-          </div>
-
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label htmlFor="description" className="ml-1">
-              {t('description')}
-            </Label>
-            <Textarea
-              type="text"
-              placeholder={t('description1')}
-              id="description"
-              rows="6"
-              required
-              onChange={handleChange}
-              value={formData.description}
-            />
-          </div>
-        </div>
-
-        <div className="flex flex-col flex-1 gap-4">
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label htmlFor="title" className="ml-1">
-              {t('title')}
-            </Label>
-            <Input
-              type="title"
-              placeholder={t('title1')}
-              id="title"
-              required
-              onChange={handleChange}
-              value={formData.title}
-            />
-          </div>
-
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label htmlFor="category" className="ml-1">
-              {t('category')}
-            </Label>
-            <select
-              id="category"
-              name="category"
-              onChange={handleChange}
-              value={formData.category}
-              className="dark:bg-[#0c0a09] cursor-pointer appearance-none flex h-9 w-full rounded-md border border-input  px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
-            >
-              <option value="All">{t('selectcategory')}</option>
-              <option value="Assistência Técnica">
-                {t('technicalassistance')}
-              </option>
-              <option value="Aulas">{t('classes')}</option>
-              <option value="Design e Tecnologia">{t('tech')}</option>
-              <option value="Eventos">{t('events')}</option>
-              <option value="Reformas">{t('reforms')}</option>
-              <option value="Serviços Domésticos">{t('homeservices')}</option>
-              <option value="Outros">{t('other')}</option>
-            </select>
-          </div>
-
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label htmlFor="cover" className="ml-1">
-              {t('coverimg')}
-            </Label>
-            <div className="flex gap-2">
+          <div className="flex flex-col flex-1 gap-4">
+            <div className="max-w-screen-xl  items-center gap-1.5">
+              <Label htmlFor="title" className="ml-1">
+                {t('title')}
+              </Label>
               <Input
-                onChange={(e) => setFiles(e.target.files)}
-                id="cover"
-                type="file"
-                accept="image/*"
+                type="title"
+                placeholder={t('title1')}
+                id="title"
+                required
+                onChange={handleChange}
+                value={formData.title}
               />
-              <Button
-                type="button"
-                disabled={coverUploading}
-                onClick={handleCoverImageSubmit}
-              >
-                {coverUploading ? 'Uploading...' : 'Upload'}
-              </Button>
             </div>
-            {coverUploading && <Progress value={progress} />}
-          </div>
+            <div className="max-w-screen-xl  items-center gap-1.5">
+              <Label htmlFor="select" className="ml-1">
+                {t('city')}
+              </Label>
+              <select
+                id="city"
+                name="city"
+                onChange={handleChange}
+                value={formData.city}
+                required
+                className="dark:bg-[#0c0a09] cursor-pointer appearance-none flex h-9 w-full rounded-md border border-input  px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+              >
+                <option value="All">{t('selectcity')}</option>
+                <option value="Pemba">Pemba</option>
+                <option value="Lichinga">Lichinga</option>
+                <option value="Nampula">Nampula</option>
+                <option value="Nacala">Nacala</option>
+                <option value="Quelimane">Quelimane</option>
+                <option value="Tete">Tete</option>
+                <option value="Moatize">Moatize</option>
+                <option value="Chimoio">Chimoio</option>
+                <option value="Beira">Beira</option>
+                <option value="Dondo">Dondo</option>
+                <option value="Maxixe">Maxixe</option>
+                <option value="Inhambane">Inhambane</option>
+                <option value="Xai-Xai">Xai-Xai</option>
+                <option value="Maputo">Maputo</option>
+                <option value="Matola">Matola</option>
+              </select>
+            </div>
+            <div className="max-w-screen-xl  items-center gap-1.5">
+              <Label htmlFor="category" className="ml-1">
+                {t('category')}
+              </Label>
+              <select
+                id="category"
+                name="category"
+                onChange={handleChange}
+                value={formData.category}
+                className="dark:bg-[#0c0a09] cursor-pointer appearance-none flex h-9 w-full rounded-md border border-input  px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+              >
+                <option value="All">{t('selectcategory')}</option>
+                <option value="Assistência Técnica">
+                  {t('technicalassistance')}
+                </option>
+                <option value="Aulas">{t('classes')}</option>
+                <option value="Design e Tecnologia">{t('tech')}</option>
+                <option value="Eventos">{t('events')}</option>
+                <option value="Reformas">{t('reforms')}</option>
+                <option value="Serviços Domésticos">{t('homeservices')}</option>
+                <option value="Outros">{t('other')}</option>
+              </select>
+            </div>
+            <div className="max-w-screen-xl  items-center gap-1.5">
+              <Label htmlFor="phone" className="ml-1">
+                {t('phone')}
+              </Label>
+              <Input
+                type="phone"
+                placeholder={t('whatsaap')}
+                id="phone"
+                required
+                onChange={handleChange}
+                value={formData.phone}
+              />
+            </div>
 
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <p className="text-red-700 text-sm">
-              {imageUploadError && imageUploadError}
-            </p>
-            {formData.coverImg !== '' && (
-              <div className="flex justify-between p-3 border items-center rounded-md">
-                <img
-                  src={formData.coverImg}
-                  alt=""
-                  className="w-20 h-20 object-contain"
+            <div className="max-w-screen-xl  items-center gap-1.5">
+              <Label htmlFor="description" className="ml-1">
+                {t('description')}
+              </Label>
+              <Textarea
+                type="text"
+                placeholder={t('description1')}
+                id="description"
+                rows="6"
+                required
+                onChange={handleChange}
+                value={formData.description}
+              />
+            </div>
+
+            <div className="max-w-screen-xl  items-center gap-1.5">
+              <Label htmlFor="cover" className="ml-1">
+                {t('coverimg')}
+              </Label>
+              <div className="flex gap-2 mb-2">
+                <Input
+                  onChange={handleCoverImageChange}
+                  id="cover"
+                  type="file"
+                  accept="image/*"
                 />
-                <Button
-                  type="button"
-                  onClick={handleRemoveCoverImage}
-                  variant="link"
-                >
-                  {t('delete')}
-                </Button>
               </div>
-            )}
-          </div>
-
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label htmlFor="images" className="ml-1">
-              {t('galleryimgs')}
-            </Label>
-            <div className="flex gap-2">
-              <Input
-                onChange={(e) => setFiles(e.target.files)}
-                type="file"
-                id="images"
-                accept="image/*"
-                multiple
-              />
-              <Button
-                type="button"
-                disabled={uploading}
-                onClick={handleImageSubmit}
-              >
-                {uploading ? 'Uploading...' : 'Upload'}
-              </Button>
+              {coverUploading && <Progress value={progress} />}
             </div>
-            {uploading && <Progress value={progress} />}
-          </div>
 
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <p className="text-red-700 text-sm">
-              {imageUploadError && imageUploadError}
-            </p>
-            {formData.imageUrls.length > 0 &&
-              formData.imageUrls.map((url, index) => (
-                <div
-                  key={url}
-                  className="flex justify-between p-3 border items-center rounded-md"
-                >
+            <div className="max-w-screen-xl  items-center gap-1.5">
+              <p className="text-red-700 text-sm">
+                {imageUploadError && imageUploadError}
+              </p>
+              {formData.coverImg !== '' && (
+                <div className="flex justify-between p-3 border items-center rounded-md">
                   <img
-                    src={url}
-                    alt="service images"
+                    src={formData.coverImg}
+                    alt=""
                     className="w-20 h-20 object-contain"
                   />
                   <Button
                     type="button"
-                    onClick={() => handleRemoveImage(index)}
+                    onClick={handleRemoveCoverImage}
                     variant="link"
                   >
                     {t('delete')}
                   </Button>
                 </div>
-              ))}
+              )}
+            </div>
 
-            <Button disabled={loading || uploading} className="mt-4 p">
-              {loading ? `${t('updating')}` : `${t('updateservice')}`}
-            </Button>
-            {error && <p className="text-red-700 text-sm">{error}</p>}
+            <div className="max-w-screen-xl  items-center gap-1.5">
+              <Label htmlFor="images" className="ml-1">
+                {t('galleryimgs')}
+              </Label>
+              <div className="flex gap-2 mb-2">
+                <Input
+                  onChange={handleGalleryImagesChange}
+                  type="file"
+                  id="images"
+                  accept="image/*"
+                  multiple
+                />
+              </div>
+              {uploading && <Progress value={progress} />}
+            </div>
+
+            <div className="max-w-screen-xl  items-center gap-1.5">
+              <p className="text-red-700 text-sm">
+                {imageUploadError && imageUploadError}
+              </p>
+              {formData.imageUrls.length > 0 &&
+                formData.imageUrls.map((url, index) => (
+                  <div
+                    key={url}
+                    className="flex justify-between p-3 border items-center rounded-md"
+                  >
+                    <img
+                      src={url}
+                      alt="service images"
+                      className="w-20 h-20 object-contain"
+                    />
+                    <Button
+                      type="button"
+                      onClick={() => handleRemoveImage(index)}
+                      variant="link"
+                    >
+                      {t('delete')}
+                    </Button>
+                  </div>
+                ))}
+
+              <Button disabled={loading || uploading} className="mt-4 p">
+                {loading ? `${t('updating')}` : `${t('updateservice')}`}
+              </Button>
+              {error && <p className="text-red-700 text-sm">{error}</p>}
+            </div>
           </div>
-        </div>
-      </form>
-    </main>
+        </form>
+      </div>
+    </section>
   );
 };
 
